f8f42822f603ae65144eb313a401014922fdb909
Andi Fajar <andifajarlah@gmail.com>
2023-02-06 21:25:51 +0800

    bari

--- app/Http/Livewire/Logmember/Deposit.php
+++ app/Http/Livewire/Logmember/Deposit.php
@@ -17,7 +17,7 @@ class Deposit extends Component
     public function render()
     {
         return view('livewire.logmember.deposit', [
-            'data' => ModelsDeposit::with('user')->when($this->member, fn($q) => $q->where('user_id', $this->member))->where('created_at', 'like', $this->date . '%')->get(),
+            'data' => ModelsDeposit::with('user')->when($this->member, fn($q) => $q->where('user_id', $this->member))->whereNotNull('processed_at')->where('created_at', 'like', $this->date . '%')->get(),
         ]);
     }
 }
